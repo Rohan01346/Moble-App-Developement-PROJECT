c1d57ef22da7d2985d8aabdba19516b65bb76b72
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-11 23:39:25 +0530

    splash_screen

--- app/src/main/java/com/example/project/welcome_page.java
+++ app/src/main/java/com/example/project/welcome_page.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
 
 public class welcome_page extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class welcome_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-    }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(welcome_page.this , MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+     }
 }
