6f9ad55d520d77ee0c2bf8a9af68c15bdbc6ba31
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-15 23:53:57 +0530

    loan

--- app/src/main/java/com/example/project/Loan_checker.java
+++ app/src/main/java/com/example/project/Loan_checker.java
@@ -2,12 +2,37 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Loan_checker extends AppCompatActivity {
-
+    Button id_check;
+    EditText id_loan_name;
+    EditText id_loan_account_number;
+    EditText id_amount_want;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_checker);
+        id_check=findViewById(R.id.check);
+        id_loan_account_number=findViewById(R.id.loan_account_number);
+        id_loan_name=findViewById(R.id.loan_name);
+        id_amount_want=findViewById(R.id.amount_want);
+        id_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loan_account_number = id_loan_account_number.getText().toString();
+                String amount_want = id_amount_want.getText().toString();
+                String loan_name = id_loan_name.getText().toString();
+                if (loan_account_number.length() == 0 || amount_want.length() == 0 || loan_name.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "fill the details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "You are Eligibile!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/project/Signup.java
+++ app/src/main/java/com/example/project/Signup.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Signup extends AppCompatActivity {
 
@@ -29,7 +30,26 @@ public class Signup extends AppCompatActivity {
                 id_signup_confirm_password=findViewById(R.id.confirmpassword);
                 id_signup_email=findViewById(R.id.email);
                 id_signup_name=findViewById(R.id.name);
-                startActivity(new Intent(Signup.this,Login_page.class));
+                id_signup_password=findViewById(R.id.password);
+                id_signup_username=findViewById(R.id.username);
+                String signup_confirm_password = id_signup_confirm_password.getText().toString();
+                String signup_email = id_signup_email.getText().toString();
+                String signup_name = id_signup_name.getText().toString();
+                String signup_password=id_signup_password.getText().toString();
+                String signup_username=id_signup_username.getText().toString();
+                if (signup_confirm_password.length() == 0 || signup_email.length() == 0 || signup_name.length() == 0 ||signup_password.length() == 0 || signup_username.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "fill the details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (signup_confirm_password != signup_password){
+                        Toast.makeText(getApplicationContext(), "password doesn't match", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Signup.this,Login_page.class));
+                    }
+                }
+
             }
         });
 
