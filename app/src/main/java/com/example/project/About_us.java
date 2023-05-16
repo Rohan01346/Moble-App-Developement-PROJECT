081002d2a8880bfb80410bb52d77ee629da9a410
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-16 13:08:53 +0530

    about_US

--- app/src/main/java/com/example/project/About_us.java
+++ app/src/main/java/com/example/project/About_us.java
@@ -2,13 +2,23 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About_us extends AppCompatActivity {
-
+    Button id_back_3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        id_back_3=findViewById(R.id.back_3);
+        id_back_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(About_us.this,Banking.class));
+            }
+        });
     }
 }
