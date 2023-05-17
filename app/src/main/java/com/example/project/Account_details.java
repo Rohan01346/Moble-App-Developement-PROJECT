303f3f8d100b5015e2c4afc09ba6b8a41e166a9c
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-17 23:35:19 +0530

    Account_details.java

--- app/src/main/java/com/example/project/Account_details.java
+++ app/src/main/java/com/example/project/Account_details.java
@@ -2,13 +2,28 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Account_details extends AppCompatActivity {
 
+    Button id_back_5;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_details);
+
+
+        id_back_5=findViewById(R.id.back_5);
+        id_back_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Account_details.this,Banking.class));
+            }
+        });
     }
 }
