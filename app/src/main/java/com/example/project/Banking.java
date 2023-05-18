db3150f3b7a207f846569a97df4347d7f7a1d508
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-19 00:15:00 +0530

    banking_database

--- app/src/main/java/com/example/project/Banking.java
+++ app/src/main/java/com/example/project/Banking.java
@@ -4,16 +4,24 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import kotlinx.coroutines.flow.SharedFlow;
 
 public class Banking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_username", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(),"Welcome"+" "+username,Toast.LENGTH_SHORT).show();
+
         setContentView(R.layout.activity_banking);
 
         CardView add_money =  findViewById(R.id.button1);
@@ -28,41 +36,3 @@ public class Banking extends AppCompatActivity {
                 startActivity(new Intent(Banking.this,Add_money.class));
             }
         });
-
-        account_details.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Banking.this,Account_details.class));
-            }
-        });
-
-        send_money.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Banking.this,Transaction.class));
-            }
-        });
-
-        loan_checker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Banking.this,Loan_checker.class));
-            }
-        });
-
-        about_us.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Banking.this,About_us.class));
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Logout",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Banking.this,Login_page.class));
-            }
-        });
-    }
-}
