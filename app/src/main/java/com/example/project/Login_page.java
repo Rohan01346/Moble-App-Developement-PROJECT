676d32166d806cd2179a8329ae515404a6ea8d01
Rudra-08 <92172059+Rudra-08@users.noreply.github.com>
2023-05-18 15:18:59 +0530

    update_login

--- app/src/main/java/com/example/project/Login_page.java
+++ app/src/main/java/com/example/project/Login_page.java
@@ -1,8 +1,9 @@
 package com.example.project;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,31 +19,50 @@ public class Login_page extends AppCompatActivity {
     EditText id_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         id_login = findViewById(R.id.loginbutton);
         id_username = findViewById(R.id.username);
         id_password = findViewById(R.id.password);
         id_signup = findViewById(R.id.signup_ask_button);
+
+        AlertDialog.Builder empty = new AlertDialog.Builder(this);
+        empty.setTitle("Sign-in Failed");
+        empty.setMessage("Fill all details.");
+        empty.setIcon(R.drawable.empty_error);
+        empty.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ;
+            }
+        });
+        AlertDialog alert = empty.create();
+
         id_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 String username = id_username.getText().toString();
                 String password = id_password.getText().toString();
-                if(username.length()==0 || password.length()==0) {
-                    Toast.makeText(getApplicationContext(),"Fill all details", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                if (username.length() == 0 || password.length() == 0) {
+                    alert.show();
+                } else {
                     Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Login_page.this, Banking.class));
                 }
             }
         });
+
         id_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login_page.this,Signup.class));
+                startActivity(new Intent(Login_page.this, Signup.class));
             }
+
         });
+
+
+
     }
+
 }
